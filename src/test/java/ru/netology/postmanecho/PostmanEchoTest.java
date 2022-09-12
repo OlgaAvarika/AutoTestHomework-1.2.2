694822a5c31cfc55c591094c66c10a9241885187
Ольга Евстифеева <olga1989nikol@rambler.ru>
2022-09-12 23:20:53 +0300

    Added a falling test

---
+++ src/test/java/ru/netology/postmanecho/PostmanEchoTest.java
@@ -0,0 +1,22 @@
+package ru.netology.postmanecho;
+
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+    @Test
+    void shouldPost() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Hello!")
+
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Hello"))
+        ;
+    }
+}
